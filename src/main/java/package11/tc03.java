c4fa8c93bee1a6aa5001a679d9e69967374fe25b
SUPREETHAJAYARAMgowda <s4supritha.gowda@gmail.com>
2024-03-07 16:56:36 +0530

    testscript is added

---
+++ src/main/java/package11/tc03.java
@@ -0,0 +1,9 @@
+package package11;
+
+public class tc03 {
+
+	public tc03() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/package11/tc04.java
@@ -0,0 +1,14 @@
+package package11;
+
+public class tc04 {
+
+	public tc04() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
